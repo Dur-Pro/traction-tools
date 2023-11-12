4111234b10787ecd4ca7b8202cdf1dadc1a9448d
Marc Durepos <marc@bemade.org>
2023-11-12 09:01:40 -0500

    Unindent traction.measurable.

--- traction/models/traction_vision.py
+++ traction/models/traction_vision.py
@@ -51,41 +51,41 @@ class TractionLevel10(models.Model):
     def _inverse_issues_headlines(self):
         pass
 
-    class TractionMeasurable(models.Model):
-        _name = 'traction.measurable'
-        _description = 'Measurable'
-        _inherit = ['mail.thread', 'mail.activity.mixin']
+class TractionMeasurable(models.Model):
+    _name = 'traction.measurable'
+    _description = 'Measurable'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-        # goal_unit = fields.Many2one('uom.uom', string='Unit of Measure')
+    # goal_unit = fields.Many2one('uom.uom', string='Unit of Measure')
 
-        name = fields.Char(string='Name')
-        vision_id = fields.Many2one(comodel_name='traction.vision',
-                                    string='Vision')
-        responsible = fields.Many2one(comodel_name='res.users',
-                                      string='Responsibility of',
-                                      index=True,
-                                      tracking=2)
-        goal = fields.Float(string='Goal')
-        goal_type = fields.Selection(
-            [
-                ('gt', 'Greater than'),
-                ('gteq', 'Greater or equal'),
-                ('eq', 'Equal to'),
-                ('lteq', 'Less or equal'),
-                ('lt', 'Less than'),
-            ],
-            string='Goal type',
-            default='eq')
+    name = fields.Char(string='Name')
+    vision_id = fields.Many2one(comodel_name='traction.vision',
+                                string='Vision')
+    responsible = fields.Many2one(comodel_name='res.users',
+                                  string='Responsibility of',
+                                  index=True,
+                                  tracking=2)
+    goal = fields.Float(string='Goal')
+    goal_type = fields.Selection(
+        [
+            ('gt', 'Greater than'),
+            ('gteq', 'Greater or equal'),
+            ('eq', 'Equal to'),
+            ('lteq', 'Less or equal'),
+            ('lt', 'Less than'),
+        ],
+        string='Goal type',
+        default='eq')
 
-        show_average = fields.Boolean(string='Show average', default=False)
-        show_cumulative = fields.Boolean(string='Show cumulative',
-                                         default=False)
-        level10_ids = fields.Many2many(comodel_name='traction.level10',
-                                       string='Level 10',
-                                       copy=False)
-        value_ids = fields.One2many(comodel_name='traction.measurable.value',
-                                    inverse_name='measurable_id',
-                                    string='Values')
+    show_average = fields.Boolean(string='Show average', default=False)
+    show_cumulative = fields.Boolean(string='Show cumulative',
+                                     default=False)
+    level10_ids = fields.Many2many(comodel_name='traction.level10',
+                                   string='Level 10',
+                                   copy=False)
+    value_ids = fields.One2many(comodel_name='traction.measurable.value',
+                                inverse_name='measurable_id',
+                                string='Values')
 
 
 class TractionMeasurableValue(models.Model):
